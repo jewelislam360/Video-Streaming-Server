602441fcf0bda471cda966f835c3e599cc9ff222
jewelislam360 <jewelislam.it360@gmail.com>
2023-08-23 01:40:30 +0600

    id get data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nn2sj3o.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +34,15 @@ async function run() {
     app.get('/allMovies', async(req, res)=>{
         const result = await allMovieCollection.find().toArray();
         res.send(result);
-    })    
+    }) 
+    
+    app.get('/allMovies/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result = await allMovieCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    })
 
 
 
